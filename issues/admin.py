9e5273cf49247c64df8c20f85e3ade108feb02e9
akkaziev_aktan <salizardfnite@gmail.com>
2026-06-22 13:30:23 +0600

    added project to admin panel. Made pagination

--- issues/admin.py
+++ issues/admin.py
@@ -1,16 +1,17 @@
 from django.contrib import admin
 
-from issues.models import Issue
-from issues.models import Status
-from issues.models import Type
-
+from issues.models import Issue, Status, Type, Project
 
 class IssueAdmin(admin.ModelAdmin):
     list_display = ("id", "summary", "status", "created_at", "updated_at")
     list_filter = ("status", "type")
     search_fields = ("summary", "description")
 
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "start_date", "end_date")
+    search_fields = ("name", "description")
 
 admin.site.register(Issue, IssueAdmin)
 admin.site.register(Status)
 admin.site.register(Type)
+admin.site.register(Project, ProjectAdmin)
