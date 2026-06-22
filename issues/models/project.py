94ff8433dead069e49053dfb73032aea0fdb1013
akkaziev_aktan <salizardfnite@gmail.com>
2026-06-22 10:50:28 +0600

    made project model

---
+++ issues/models/project.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Projects(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Название")
+    description = models.TextField(max_length=1000, verbose_name="Описание")
+    start_date = models.DateField(verbose_name="Дата начала")
+    end_date = models.DateField(verbose_name="Дата окончания", null=True, blank=True)
+
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = "projects"
+        verbose_name = "Проект"
+        verbose_name_plural = "Проекты"
