255db39d1a7f1900ad33855d0621a89adc74167e
akkaziev_aktan <salizardfnite@gmail.com>
2026-06-22 11:03:16 +0600

    deleted null=True, blank=True from issues.py and made migration

--- issues/models/issue.py
+++ issues/models/issue.py
@@ -14,7 +14,7 @@ class Issue(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Время обновления")
     project = models.ForeignKey("issues.Project",on_delete=models.RESTRICT,
-                                verbose_name="Проект",related_name="issues",null=True,blank=True,)
+                                verbose_name="Проект", related_name="issues")
 
     def __str__(self):
         return self.summary
