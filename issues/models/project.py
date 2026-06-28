09c76c8b4bdec5a95064f70180ae1f1a263e71d4
akkaziev_aktan <salizardfnite@gmail.com>
2026-06-29 00:04:55 +0600

    added many to many field to project model

--- issues/models/project.py
+++ issues/models/project.py
@@ -1,10 +1,15 @@
 from django.db import models
 
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 class Project(models.Model):
     name = models.CharField(max_length=100, verbose_name="Название")
     description = models.TextField(max_length=1000, verbose_name="Описание")
     start_date = models.DateField(verbose_name="Дата начала")
     end_date = models.DateField(verbose_name="Дата окончания", null=True, blank=True)
+    members = models.ManyToManyField( User, related_name="projects", verbose_name="Пользователи")
 
 
     def __str__(self):
