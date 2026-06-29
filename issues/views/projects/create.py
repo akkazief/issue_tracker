fac549fde52c78ef14503dff101caac150dba46a
akkaziev_aktan <salizardfnite@gmail.com>
2026-06-29 10:52:53 +0600

    added exit buttons, made bonus when creating project logged user already in project

--- issues/views/projects/create.py
+++ issues/views/projects/create.py
@@ -8,3 +8,8 @@ class CreateProjectView(LoginRequiredMixin, CreateView):
     template_name = "projects/create_project.html"
     form_class = ProjectForm
     success_url = reverse_lazy("projects_list")
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        self.object.members.add(self.request.user)
+        return response
